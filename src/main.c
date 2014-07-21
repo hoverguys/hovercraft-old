44c8c14cd5c3ee4425377118c8339732b319a4c4
zeroZshadow <martijn_gerkes@hotmail.com>
2014-07-21 19:08:01 +0200

    Added menu music and modplay support

--- src/main.c
+++ src/main.c
@@ -5,25 +5,36 @@
 #include <ogcsys.h>
 #include <gccore.h>
 
+// include generated header
+// rename from modfile.mod to modfile_mod.h
+#include "menumusic_mod.h"
+
+// Audio
+#include <aesndlib.h>
+#include <gcmodplay.h>
+
 static void *xfb = NULL;
 static GXRModeObj *rmode = NULL;
+static MODPlay play;
 
-void *Initialise();
+void *initialise();
 
 int main(int argc, char **argv) {
 
-	xfb = Initialise();
+	xfb = initialise();
+
+	playMod();
 
 	printf("\nHello World!\n");
 
-	while(1) {
+	while (1) {
 
 		VIDEO_WaitVSync();
 		PAD_ScanPads();
 
 		int buttonsDown = PAD_ButtonsDown(0);
-		
-		if( buttonsDown & PAD_BUTTON_A ) {
+
+		if (buttonsDown & PAD_BUTTON_A) {
 			printf("Button A pressed.\n");
 		}
 
@@ -35,25 +46,32 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
-void * Initialise() {
+void* initialise() {
 
 	void *framebuffer;
 
 	VIDEO_Init();
 	PAD_Init();
-	
+	AESND_Init(NULL);
+
 	rmode = VIDEO_GetPreferredMode(NULL);
 
 	framebuffer = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-	console_init(framebuffer,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
-	
+	console_init(framebuffer, 20, 20, rmode->fbWidth, rmode->xfbHeight, rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+
 	VIDEO_Configure(rmode);
 	VIDEO_SetNextFramebuffer(framebuffer);
 	VIDEO_SetBlack(FALSE);
 	VIDEO_Flush();
 	VIDEO_WaitVSync();
-	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+	if (rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
 
 	return framebuffer;
 
+}
+
+void playMod() {
+	MODPlay_Init(&play);
+	MODPlay_SetMOD(&play, menumusic_mod);
+	MODPlay_Start(&play);
 }
