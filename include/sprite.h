beeb4614dfda0cfef2e00d43c03ce1b7876bbff7
zeroZshadow <martijn_gerkes@hotmail.com>
2015-08-02 19:28:05 +0200

    Updated sprite code to revision version

--- include/sprite.h
+++ include/sprite.h
@@ -12,41 +12,50 @@
 typedef struct {
 	GXTexObj*   texture;
 	transform_t transform;
-	f32         UVoffsetX, UVoffsetY;
-	f32         UVwidth, UVheight;
+	f32 width, height;
+	GXColor color;
 } sprite_t;
 
-/*! \brief Initialize sprite system, including 2D camera
- */
-void SPRITE_init();
-
 /*! \brief Create empty sprite
- *  \return Pointer to newly sprite structure
- */
-sprite_t* SPRITE_create();
+*  \return Pointer to newly sprite structure
+*/
+sprite_t* SPRITE_create(f32 x, f32 y, f32 depth, f32 width, f32 height, GXTexObj* texture);
 
 /*! \brief Frees a sprite
- *  \param sprite Sprite to destroy
- */
+*  \param sprite Sprite to destroy
+*/
 void SPRITE_free(sprite_t* sprite);
 
 /*! \brief Assign a texture to a sprite, including offset/size
- *  \param sprite     Sprite to assign texture to
- *  \param texture    Texture object to use
- *  \param position   UV offset/position
- *  \param texSize    UV total size
- *  \param spriteSize Sprite size in the texture
- */
-void SPRITE_setTexture(sprite_t* sprite, GXTexObj* texture, f32 position[2], f32 spriteSize[2], TexSize texSize);
+*  \param sprite     Sprite to assign texture to
+*  \param texture    Texture object to use
+*  \param position   UV offset/position
+*  \param texSize    UV total size
+*  \param spriteSize Sprite size in the texture
+*/
+void SPRITE_setTexture(sprite_t* sprite, GXTexObj* texture);
 
-/*! \brief Renders a sprite on screen 
- *  \param sprite Sprite to render
- */
+/*! \brief Renders a sprite on screen
+*  \param sprite Sprite to render
+*/
 void SPRITE_render(sprite_t* sprite);
 
 /*! \brief Force Matrix regeneration o sprite
 *  \param sprite Sprite to process
 */
 void SPRITE_flush(sprite_t* sprite);
+
+/*! \brief Move a sprite to a specified position
+*  \param object Object to move
+*  \param tX     X coordinate
+*  \param tY     Y coordinate
+*  \param tDepth Depth
+*/
+void SPRITE_moveTo(sprite_t* sprite, const f32 tX, const f32 tY, const f32 tDepth);
+
+
+void SPRITE_color(sprite_t* sprite, GXColor color);
+
+void SPRITE_scaleTo(sprite_t* sprite, const f32 sX, const f32 sY, const f32 sZ);
 
 #endif
--- src/sprite.c
+++ src/sprite.c
@@ -2,29 +2,31 @@
 #include "mathutil.h"
 #include <malloc.h>
 
-sprite_t* SPRITE_create() {
+sprite_t* SPRITE_create(f32 x, f32 y, f32 depth, f32 width, f32 height, GXTexObj* texture) {
 	sprite_t* sprite = malloc(sizeof(sprite_t));
-	return sprite;
-}
-
-void SPRITE_setTexture(sprite_t* sprite, GXTexObj* texture, f32 position[2], f32 spriteSize[2], TexSize texSize) {
+	sprite->width = width;
+	sprite->height = height;
 	sprite->texture = texture;
-	f32 texRepr = 1.0f / texSize;
-	f32 res[2] = { 0, 0 };
-	f32 size[2] = { texRepr, texRepr };
-	ps_float2Mul(position, size, res);
-	sprite->UVoffsetX = res[0];
-	sprite->UVoffsetY = res[1];
-	ps_float2Mul(spriteSize, size, res);
-	sprite->UVwidth = res[0];
-	sprite->UVheight = res[1];
-	sprite->transform.position = (guVector) { 0, 0, 0 };
-	sprite->transform.scale = (guVector) { 1, 1, 1 }; 
+	sprite->color = (GXColor) { 0xff, 0xff, 0xff, 0xff };
+
+	guMtxIdentity(sprite->transform.matrix);
+
+	sprite->transform.position = (guVector) { x, y, depth };
+	sprite->transform.scale = (guVector) { 1, 1, 1 };
 	guQuaternion rotation;
 	EulerToQuaternion(&rotation, 0, 0, 0);
 	sprite->transform.rotation = rotation;
+	sprite->transform.dirty = TRUE;
 
-	MakeMatrix(&sprite->transform);
+	return sprite;
+}
+
+void SPRITE_color(sprite_t* sprite, GXColor color) {
+	sprite->color = color;
+}
+
+void SPRITE_setTexture(sprite_t* sprite, GXTexObj* texture) {
+	sprite->texture = texture;
 }
 
 void SPRITE_flush(sprite_t* sprite) {
@@ -39,30 +41,65 @@ void SPRITE_render(sprite_t* sprite) {
 	/* Check dirty flag */
 	SPRITE_flush(sprite);
 
-	/* Set position to identity */
-	Mtx viewMtx, modelviewMtx;
-	guMtxIdentity(viewMtx);
-	guMtxConcat(viewMtx, sprite->transform.matrix, modelviewMtx);
-	GX_LoadPosMtxImm(modelviewMtx, GX_PNMTX0);
+	/* Set position */
+	Mtx dummy;
+	guMtxIdentity(dummy);
+	GX_LoadPosMtxImm(sprite->transform.matrix, GX_PNMTX0);
+	GX_LoadNrmMtxImm(dummy, GX_PNMTX0); //No dummies required
+
+										/* Set sprite texture */
+	GX_LoadTexObj(sprite->texture, GX_TEXMAP0);
+
+	/* Set color and disable lighting*/
+	GX_SetChanCtrl(GX_COLOR0A0, GX_DISABLE, GX_SRC_REG, GX_SRC_REG, GX_LIGHT0, GX_DF_CLAMP, GX_AF_NONE);
+	GX_SetChanMatColor(GX_COLOR0A0, sprite->color);
+
+	/* Vtx descriptors reset and set */
+	GX_ClearVtxDesc();
+	GX_SetVtxDesc(GX_VA_POS, GX_DIRECT);
+	GX_SetVtxDesc(GX_VA_TEX0, GX_DIRECT);
+	GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XY, GX_F32, 0);
+	GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_TEX0, GX_TEX_ST, GX_F32, 0);
+
+	/* Lighting off, Alpha blend */
+	GX_SetNumChans(2);
 
 	/* Orthographic mode */
 	GXU_2DMode();
 
 	GX_Begin(GX_QUADS, GX_VTXFMT0, 4);
-	
+
 	/* Top left */
 	GX_Position2f32(0, 0);
-	GX_TexCoord2f32(sprite->UVoffsetX, sprite->UVoffsetY);
+	GX_TexCoord2f32(0, 0);
+
 	/* Bottom left */
-	GX_Position2f32(0, sprite->UVheight);
-	GX_TexCoord2f32(sprite->UVoffsetX, sprite->UVoffsetY + sprite->UVheight);
+	GX_Position2f32(0, sprite->height);
+	GX_TexCoord2f32(0, 1);
+
 	/* Bottom right */
-	GX_Position2f32(sprite->UVwidth, sprite->UVheight);
-	GX_TexCoord2f32(sprite->UVoffsetX + sprite->UVwidth, sprite->UVoffsetY + sprite->UVheight);
+	GX_Position2f32(sprite->width, sprite->height);
+	GX_TexCoord2f32(1, 1);
+
 	/* Top right */
-	GX_Position2f32(sprite->UVwidth, 0);
-	GX_TexCoord2f32(sprite->UVoffsetX + sprite->UVwidth, sprite->UVoffsetY);
+	GX_Position2f32(sprite->width, 0);
+	GX_TexCoord2f32(1, 0);
 
 	GX_End();
 }
 
+void SPRITE_moveTo(sprite_t* sprite, const f32 tX, const f32 tY, const f32 tDepth) {
+	transform_t* t = &sprite->transform;
+	t->position.x = tX;
+	t->position.y = tY;
+	t->position.z = tDepth;
+	t->dirty = TRUE;
+}
+
+void SPRITE_scaleTo(sprite_t* sprite, const f32 sX, const f32 sY, const f32 sZ) {
+	transform_t* t = &sprite->transform;
+	t->scale.x = sX;
+	t->scale.y = sY;
+	t->scale.z = sZ;
+	t->dirty = TRUE;
+}
